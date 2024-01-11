5363e65b4abb04ae304c5bee656f49b5cb1aa77e
Felix chinonso Emmanuel <Feelchi1star@gmail.com>
2024-01-12 00:26:26 +0100

    Added Clipboard support

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -1,3 +1,4 @@
+import ListtItem from "@/components/ListtItem";
 import axios from "axios";
 
 const fetchData = async (p) => {
@@ -30,21 +31,12 @@ export default async function ADMIN({ searchParams: { password } }) {
 
       <div className="border p-4 rounded-lg">
         <h1 className="font-bold text-xl">
-          Paid Group Members: {data?.userLength}+
+          Paid Group Members: {data?.userLength}
         </h1>
 
         <ul className="px-1 overflow-y-auto max-h-screen">
           {data?.user?.map((e, idx) => {
-            return (
-              <li
-                className="flex p-2 shadow-sm bg-stone-100 my-2 rounded-md flex-col gap-1"
-                key={idx}
-              >
-                <h4 className="font-bold">{e.name}</h4>
-                <p className="text-sm">{e.matricNumber}</p>
-                <p className="text-sm">{e.email}</p>
-              </li>
-            );
+            return <ListtItem e={e} key={idx} />;
           })}
         </ul>
       </div>
---
+++ src/components/ListtItem.js
@@ -0,0 +1,37 @@
+"use client";
+import React from "react";
+
+const ListtItem = ({ e }) => {
+  const [copied, setCopied] = React.useState(false);
+  React.useEffect(() => {
+    const timer = setTimeout(() => {
+      setCopied(false);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, [copied]);
+  return (
+    <li
+      onClick={() => {
+        navigator.clipboard
+          .writeText(e.matricNumber.toLowerCase())
+          .then(() => {
+            setCopied(true);
+          })
+          .catch((e) => alert("Failed to copy to ClipBoard"));
+      }}
+      className="flex p-2 cursor-copy shadow-sm bg-stone-100 my-2 rounded-md flex-col gap-1"
+    >
+      <h4 className="font-bold">{e.name}</h4>
+      <p
+        className={`text-sm transition-all delay-200 ease-in ${
+          copied ? "text-green-500" : ""
+        }`}
+      >
+        {copied ? "copied" : e.matricNumber}
+      </p>
+      <p className="text-sm">{e.email}</p>
+    </li>
+  );
+};
+
+export default ListtItem;
